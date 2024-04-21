e7f49a1c6c1a4f83ec7d4330287bf9649863ef9b
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-21 11:18:34 -0300

    Create rascunho_dois.py

---
+++ rascunhos/rascunho_dois.py
@@ -0,0 +1,6 @@
+print("Calcule a média de três números.")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+media = (num1 + num2 + num3) / 3
+print(f'A média é: {media}')
