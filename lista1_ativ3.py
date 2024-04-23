a6590df4d20e2288b427ac25c284495e13dba88c
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-23 11:14:42 -0300

    Update lista1_ativ3.py

--- lista1_ativ3.py
+++ lista1_ativ3.py
@@ -7,4 +7,16 @@ pontuacaoMax = int(input())
 
 if pontuacaoCharles == 0:
     print("Oxe! E vai morrer por causa de 25 pontos?")
-    
+elif pontuacaoCharles == 25:
+    print("O meu favorito venceu! Pode torar o aco Verstappen")
+elif pontuacaoCharles == 18 or pontuacaoCharles == 15:
+    print("Esse Charles eh arretado mesmo")
+elif pontuacaoCharles == 12 or pontuacaoCharles == 10:
+    print("Ele eh desenrolado demais")
+
+if pontuacaoCharles >= (pontuacaoMax - 4) and pontuacaoCharles < pontuacaoMax:
+     print("Ta com a molestia, vai perder para Max Verstappen eh")
+elif pontuacaoCharles <= (pontuacaoMax + 4) and pontuacaoCharles > pontuacaoMax:
+    print("Ta com a molestia, vai perder para Max Verstappen eh")
+elif pontuacaoCharles > (pontuacaoMax + 4):
+    print("Deu o sangue")
