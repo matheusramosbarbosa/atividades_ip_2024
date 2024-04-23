35249df6240c90cdcb13f9754587b7e90335378b
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-23 11:18:33 -0300

    Add files via upload

---
+++ rascunhos/rascunho_tres.py
@@ -0,0 +1,9 @@
+#Atividade 3 da Lista 1
+#colocação de 1 a 10, mas as condições não exigem a lista
+#é o bra do bra
+
+pontuacaoCharles = int(input())
+pontuacaoMax = int(input())
+
+if pontuacaoCharles >= (pontuacaoMax - 4) and pontuacaoCharles < pontuacaoMax:
+     print("Ta com a molestia, vai perder para Max Verstappen eh")
