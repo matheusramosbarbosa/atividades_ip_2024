21d5fb56f084b27358b2c271eec4d81d2d908654
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-23 09:58:03 -0300

    Create lista1_ativ3.py

---
+++ lista1_ativ3.py
@@ -0,0 +1,10 @@
+#Atividade 3 da Lista 1
+#colocação de 1 a 10, mas as condições não exigem a lista
+#é o bra do bra
+
+pontuacaoCharles = int(input())
+pontuacaoMax = int(input())
+
+if pontuacaoCharles == 0:
+    print("Oxe! E vai morrer por causa de 25 pontos?")
+    
