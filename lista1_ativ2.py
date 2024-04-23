2bff7b9d17efb260a938d41730afc2d1c1ca86d5
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-23 09:51:18 -0300

    Create lista1_ativ2.py

---
+++ lista1_ativ2.py
@@ -0,0 +1,24 @@
+#Atividade 2 da Lista 1
+#nota
+#-----------------------------------------------
+# pistas: Mônaco, Ímola e Spa-Francorchamps
+#ordem: Mônaco interior à Ímola, Ímola anterior à Spa-Francorchamps
+
+velocidade_maxima = int(input())
+tempo = input()
+sol = "ensolarado"
+chuva = "chuvoso"
+nevoa = "neblina"
+
+if velocidade_maxima >= 250 and velocidade_maxima <= 260 and (tempo == sol or tempo == chuva):
+    print("Nesse dia, Senna corria em Mônaco, onde esteve no pódio 8 vezes!")
+elif velocidade_maxima < 250 and tempo == nevoa:
+    print("Nesse dia, Senna corria em Mônaco, onde esteve no pódio 8 vezes!")
+elif velocidade_maxima >= 261 and velocidade_maxima <= 285 and (tempo == sol or tempo == chuva):
+    print("Senna corria em Ímola, onde infelizmente fez sua última corrida.")
+elif velocidade_maxima < 260 and tempo == nevoa:
+    print("Senna corria em Ímola, onde infelizmente fez sua última corrida.")
+elif velocidade_maxima >= 286 and velocidade_maxima <= 310 and (tempo == sol or tempo == chuva):
+    print("Claramente Spa-Francorchamps, circuito no qual Senna venceu histórico duelo com Prost depois de três largadas!")
+elif velocidade_maxima < 285 and tempo == nevoa:
+    print("Claramente Spa-Francorchamps, circuito no qual Senna venceu histórico duelo com Prost depois de três largadas!")
