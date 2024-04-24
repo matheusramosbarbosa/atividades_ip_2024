84e0a2ff0f67247426b14602b8ce8ec8849043f8
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-24 11:48:40 -0300

    Update lista1_ativ3.py

--- lista1_ativ3.py
+++ lista1_ativ3.py
@@ -1,10 +1,10 @@
 #Atividade 3 da Lista 1
-#colocação de 1 a 10, mas as condições não exigem a lista
-#é o bra do bra
 
+#Inputs exigidos pelo desafio, com a pontuação de Charles e a pontuação de Max
 pontuacaoCharles = int(input())
 pontuacaoMax = int(input())
 
+#Condição para definir qual a mensagem que vai para o output, de acordo com a pontuação do Charles
 if pontuacaoCharles == 0:
     print("Oxe! E vai morrer por causa de 25 pontos?")
 elif pontuacaoCharles == 25:
@@ -14,6 +14,8 @@ elif pontuacaoCharles == 18 or pontuacaoCharles == 15:
 elif pontuacaoCharles == 12 or pontuacaoCharles == 10:
     print("Ele eh desenrolado demais")
 
+#Condição para definir se Charles possui uma diferença de 4 pontos do Max, para mais ou para menos
+#Print caso Charles possua a diferença nessa faixa, ou, caso possua uma diferença bem maior em comparação a Max
 if pontuacaoCharles >= (pontuacaoMax - 4) and pontuacaoCharles < pontuacaoMax:
      print("Ta com a molestia, vai perder para Max Verstappen eh")
 elif pontuacaoCharles <= (pontuacaoMax + 4) and pontuacaoCharles > pontuacaoMax:
