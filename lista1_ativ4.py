7d801779d2438f7babad4ed5a06cbe00cd133178
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-24 11:49:09 -0300

    Update lista1_ativ4.py

--- lista1_ativ4.py
+++ lista1_ativ4.py
@@ -1,5 +1,6 @@
-#Ordem alfabética: Lewis, Max e Valtteri
+#Atividade 4 da lista 1
 
+#Aqui encontram-se os inputs pedidos pelo desafio, onde p_{piloto} é a pontuação do piloto, e c_{piloto} é a colocação do piloto.
 p_lewis = int(input())
 c_lewis = int(input())
 p_max = int(input())
@@ -7,6 +8,7 @@ c_max = int(input())
 p_valtteri = int(input())
 c_valtteri = int(input())
 
+#Condição para definir as pontuações de acordo com a colocação do piloto, adicionando 5 pontos se no top 10.
 if c_lewis <= 10:
     p_lewis = p_lewis + 5
 if c_max <= 10:
@@ -14,33 +16,29 @@ if c_max <= 10:
 if c_valtteri <= 10:
     p_valtteri = p_valtteri + 5
 
-
+#Variáveis de ajuda, para definir o vencedor final (seu nome) e a pontuação do vencedor final.
 vencedor = ""
 p_vencedor = 0
 
+#Condição para definição de vencedor e atribuição da pontuação do vencedor à variável de ajuda.
 if (p_lewis == p_max) and (p_lewis > p_valtteri):
-    # print(f"Lewis Hamilton ganhou a competição com {p_lewis} pontos!")
     vencedor = "Lewis Hamilton"
     p_vencedor = p_lewis
 elif (p_max == p_valtteri) and (p_max > p_lewis):
     vencedor = "Max Verstappen"
     p_vencedor = p_max
-    # print(f"Max Verstappen ganhou a competição com {p_max} pontos!")
 elif (p_max > p_valtteri) and (p_max > p_lewis):
     vencedor = "Max Verstappen"
     p_vencedor = p_max
-    # print(f"Max Verstappen ganhou a compteição com {p_max} pontos!")
 elif (p_valtteri > p_max) and (p_valtteri > p_lewis):
     vencedor = "Valtteri Bottas"
     p_vencedor = p_valtteri
-    # print(f"Valtteri Bottas ganhou a competição com {p_valtteri} pontos!")
 elif (p_lewis > p_max) and (p_lewis > p_valtteri):
     vencedor = "Lewis Hamilton"
     p_vencedor = p_lewis
-    # print(f"Lewis Hamilton ganhou a compteição com {p_lewis} pontos!")
 elif p_lewis == p_valtteri == p_max:
     vencedor = "Lewis Hamilton"
     p_vencedor = p_lewis
-    # print(f"Lewis Hamilton ganhou a competição com {p_lewis} pontos!")
 
+#Output final, print utilizando as variáveis de apoio.
 print(f"{vencedor} ganhou a competição com {p_vencedor} pontos!")
