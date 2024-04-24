5a49b8915bda062092c169991ed32323feef8f68
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-24 11:48:21 -0300

    Update lista1_ativ2.py

--- lista1_ativ2.py
+++ lista1_ativ2.py
@@ -1,15 +1,17 @@
 #Atividade 2 da Lista 1
-#nota
-#-----------------------------------------------
-# pistas: Mônaco, Ímola e Spa-Francorchamps
+#pistas: Mônaco, Ímola e Spa-Francorchamps
 #ordem: Mônaco interior à Ímola, Ímola anterior à Spa-Francorchamps
 
+#Inputs exigidos pelo desafio
 velocidade_maxima = int(input())
 tempo = input()
+
+#Variáveis de apoio para ajudar na organização das condições, definindo o clima
 sol = "ensolarado"
 chuva = "chuvoso"
 nevoa = "neblina"
 
+#Condições para definir em qual pista Senna corria nesse dia
 if velocidade_maxima >= 250 and velocidade_maxima <= 260 and (tempo == sol or tempo == chuva):
     print("Nesse dia, Senna corria em Mônaco, onde esteve no pódio 8 vezes!")
 elif velocidade_maxima < 250 and tempo == nevoa:
