3ca3b882e0664e616f36061c4bdca33b164881c8
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-24 11:48:02 -0300

    Update lista1_ativ1.py

--- lista1_ativ1.py
+++ lista1_ativ1.py
@@ -1,15 +1,17 @@
 #Atividade 1 da Lista 1
-#é o brasil do brasil
+
+#Inputs exigidos pelo desafio
 piloto = input()
 distancia = float(input())
 tempo = float(input())
 
+#Cálculo da velocidade média
 v_media = distancia / tempo
 
+#Condições para definir qual foi o comentário sobre o rendimento do piloto
 if v_media > 227:
     print(f'{piloto} se deu muito bem na prova de hoje!!')
 elif v_media == 227:
     print(f'{piloto} teve um ótimo resultado. Mas, acredito que temos potencial para melhorar um pouco mais!')
 else:
     print(f'{piloto} não se deu tão bem. É preciso melhorar isso!')
-
