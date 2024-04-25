364a8c41541006cc0cb876100b1d6819c9917e95
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-25 12:09:26 -0300

    Atualização de pasta

---
+++ anotacoes/aulaCinco.py
@@ -0,0 +1,21 @@
+#Exercício 1
+
+#Escreva um programa para ler dois valores reais, x e y, correspondentes ás coordenadas de um ponto no plano e dizer em que quadrante se encotnra, ou se está no eixo-x ou no eixo-y ou se está na origem.
+
+x = float(input('Ponha o valor de x: '))
+y = float(input('Ponha o valor de y: '))
+
+if (x and y) == 0:
+    print('Você está na origem.')
+elif x == 0:
+    print('Você está no eixo y!')
+elif y == 0:
+    print('Você está no eixo x!')
+elif y > 0 and x > 0:
+    print('Você está no 1º quadrante!')
+elif y > 0 and x < 0:
+    print('Você está no 2º qudrante!')
+elif y < 0 and x < 0:
+    print('Você está no 3º quadrante!')
+elif y < 0 and x > 0:
+    print('Você está no 4º quadrante!') 
