d0c3b2be721677dfcfa1a33dd186f4e94df81362
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-04-25 10:20:05 -0300

    Create lista1_ativ5.py

---
+++ lista_1/lista1_ativ5.py
@@ -0,0 +1,80 @@
+#Atividade 5 da lista 1
+
+#Variáveis de input e vazias (de suporte)
+frase_01 = input()
+frase_02 = input()
+direcao = ""
+retorno_01 = ""
+retorno_02 = ""
+retorno_direcao = ""
+retorno_direcao2 = ""
+
+#Condição para caso o usuário ponha mensagem de curva
+curva = "Tem uma curva vindo aí, me ajuda!"
+if frase_01 == curva:
+    direcao = input()
+elif frase_02 == curva:
+    direcao = input()
+
+#Variáveis que armazenam as frases (para simplificar mais as condicionais)
+pisa = "PISA FUNDO"
+ultrapassa = "Ultrapassa ele agora!"
+freia = "FREIA AGORA E ME DIZ PARA QUE LADO É"
+entao = "ENTÃO VIRA LOGO!"
+virar = "É SÓ VIRAR!"
+ajuda = "Assim não tem como te ajudar, amiga"
+amiga = "Amiga, calma! Tem um pit stop na tua frente..."
+eita = "Eita, não entendi nada..."
+lets = "LET'S RIDE!"
+
+
+#Condições para a primeira frase
+if frase_01 == "TÔ EM ÚLTIMO!":
+    retorno_01 = pisa
+elif frase_01 == "Esse cara não sai da minha frente...":
+    retorno_01 = ultrapassa
+elif frase_01 == curva:
+    retorno_01 = freia
+    if direcao == "DIREITA":
+        retorno_direcao = entao
+    elif direcao == "ESQUERDA":
+        retorno_direcao = virar
+    else:
+        retorno_direcao = ajuda
+elif frase_01 == "MEU PNEU FUROU SOCORRO!":
+    retorno_01 = amiga
+else:
+    retorno_01 = eita
+
+#Condições para a segunda frase
+if frase_02 == "TÔ EM ÚLTIMO!":
+    retorno_02 = pisa
+elif frase_02 == "Esse cara não sai da minha frente...":
+    retorno_02 = ultrapassa
+elif frase_02 == curva:
+    retorno_02 = freia
+    if direcao == "DIREITA":
+        retorno_direcao2 = entao
+    elif direcao == "ESQUERDA":
+        retorno_direcao2 = virar
+    else:
+        retorno_direcao2 = ajuda
+elif frase_02 == "MEU PNEU FUROU SOCORRO!":
+    retorno_02 = amiga
+else:
+    retorno_02 = eita
+
+#Condições para os outputs finais
+if frase_01 == curva:
+    print(f"{retorno_01}")
+    print(f"{retorno_direcao}")
+else:
+    print(f"{retorno_01}")
+
+if frase_02 == curva:
+    print(f"{retorno_02}")
+    print(f"{retorno_direcao2}")
+else:
+    print(f"{retorno_02}")
+
+print(f"{lets}")
