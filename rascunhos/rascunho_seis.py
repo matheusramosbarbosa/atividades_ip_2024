ce939deb32a82cc03925019b2eca6f078fb73500
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-05-08 11:50:17 -0300

    Update rascunho_seis.py

--- rascunhos/rascunho_seis.py
+++ rascunhos/rascunho_seis.py
@@ -1,16 +1,26 @@
-from random import radint #random é a biblioteca e radint é a função importada da biblioteca
+# from random import radint #random é a biblioteca e radint é a função importada da biblioteca
 
-# gera número inteiro aleatório entre 0 e 100
-num = radint(0, 100)
-num
+# # gera número inteiro aleatório entre 0 e 100
+# num = radint(0, 100)
+# num
 
-num_escolhido = int(input("Escolha um número"))
-while (num_escolhido != num):
-  if num_escolhido > num:
-    print("O numero sorteado é menor!")
-    num_escolhido = int(input("Escolha um número"))
-  elif (num_escolhido < num):
-    print("O número sorteado é maior!")
-    num_escolhido = int(input("Escolha um número"))
-else:
-  print("Parabéns! Você acertou o número!")
+# num_escolhido = int(input("Escolha um número"))
+# while (num_escolhido != num):
+#   if num_escolhido > num:
+#     print("O numero sorteado é menor!")
+#     num_escolhido = int(input("Escolha um número"))
+#   elif (num_escolhido < num):
+#     print("O número sorteado é maior!")
+#     num_escolhido = int(input("Escolha um número"))
+# else:
+#   print("Parabéns! Você acertou o número!")
+
+x = "fui a praia"
+y = "comi sushi"
+print(x + y)
+
+num = 1
+while (num <= 100):
+    var1 = num
+    print(num)
+    num += 1
