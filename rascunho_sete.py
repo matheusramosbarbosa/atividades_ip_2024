d471b23adc08ee12a5d0437c7848d036c13f82d1
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-05-10 10:33:04 -0300

    Create rascunho_sete.py

---
+++ rascunho_sete.py
@@ -0,0 +1,18 @@
+numero_inteiro = int(input("Digite um número inteiro maior que 0 e menor que 100: "))
+variavel_apoio = ""
+while ((numero_inteiro < 0) or (numero_inteiro > 100)):
+    print("Desculpe, esse número é menor que 0 ou maior que 100!")
+    numero_inteiro = int(input("Digite um número inteiro maior que 0 e menor que 100: "))
+
+while ((numero_inteiro > 0) and (numero_inteiro < 99)):
+    if numero_inteiro % 2 == 0:
+        numero_inteiro += 1
+        variavel_apoio += f"{numero_inteiro}, "
+    elif (numero_inteiro % 2 == 1) and (numero_inteiro <= 95):
+        numero_inteiro += 2
+        variavel_apoio += f"{numero_inteiro}, "
+    elif (numero_inteiro == 97):
+        numero_inteiro += 2
+        variavel_apoio += f"{numero_inteiro}."
+else:
+    print(variavel_apoio)
