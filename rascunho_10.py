8f92984afd1ca690362bcf8e9614ffe5f933e8ab
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-05-10 11:41:05 -0300

    Update rascunho_10.py

--- rascunho_10.py
+++ rascunho_10.py
@@ -1,6 +1,10 @@
 numero = int(input("Digite um número maior que zero e menor que 100: "))
+while ((numero < 0) or (numero > 100)):
+  numero = int(input("Digite um número maior que zero e menor que 100: "))
 
+if numero % 2 == 0:
+  numero += 1
 for numero in range (numero, 100, 2):
     print(f'{numero},', end=' ')
 else:
-    print("Fim!")
+    print(f'{numero}.')
