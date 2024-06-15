f77917b04c1add2657b54663e760b6f706695e97
matheusramosbarbosa <167692238+matheusramosbarbosa@users.noreply.github.com>
2024-06-14 23:16:05 -0300

    oii

---
+++ lista_4/lista4_ativ1.py
@@ -0,0 +1,2 @@
+# Atividade 1 da Lista 4
+
---
+++ rascunhos/rascunho_36.py
@@ -0,0 +1,5 @@
+numero = int(input('Escreva o número: '))
+resultado = numero // 8
+resto = numero % 8
+
+print(f'Resultado: {resultado}\nResto: {resto}')
